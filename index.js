26d8ea92cd85d013839370260f697d0c384a073e
jurvantoledo <jurvantoledo@live.nl>
2021-01-04 10:52:07 +0100

    made express server and auth route

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const corsMiddleware = require("cors")
+const authRouter = require("./routers/auth")
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 4000;
+
+
+const app = express()
+
+app.use(corsMiddleware())
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
+
+if (process.env.DELAY) {
+    app.use((req, res, next) => {
+      setTimeout(() => next(), parseInt(process.env.DELAY));
+    });
+  }
+
+  app.use("/", authRouter);
+
+// default is port 4000
+  app.listen(PORT, () => {
+      console.log(`Listening on port: ${PORT}`)
+  })
