d985fde1eef9c119729bcb4098bd750588dcef84
jurvantoledo <jurvantoledo@live.nl>
2021-01-04 15:22:41 +0100

    add order route

--- routers/order.js
+++ routers/order.js
@@ -1,6 +1,8 @@
 const { Router } = require("express")
 const Order = require("../models").order
 const ArtWork = require("../models").artWork
+const User = require("../models").user
+const Gallery = require("../models").gallery
 
 const router = new Router()
 
@@ -12,6 +14,25 @@ router.get("/", async (req, res, next) => {
         })
         res.status(200).send({message: "ok", orders})   
 
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const orders = await Order.findByPk(id, {
+            include : [
+            {
+            model: ArtWork,
+            include: [{ model: Gallery, attributes: ["name"] }],
+            attributes: ["name"],
+            },
+            User
+        ],
+        })
+        res.status(200).send({message: "ok", orders})
     } catch (error) {
         next(error)
     }
