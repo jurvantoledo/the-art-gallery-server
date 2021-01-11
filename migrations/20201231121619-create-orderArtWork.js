1bc9c906725bef6acc643ebcddaa7ea657e8ee30
jurvantoledo <jurvantoledo@live.nl>
2021-01-11 16:04:30 +0100

    models and migrations fixed

--- migrations/20201231121619-create-orderArtWork.js
+++ migrations/20201231121619-create-orderArtWork.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('orderArtWork', {
+    await queryInterface.createTable('orderArtWorks', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -37,6 +37,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('orderArtWork');
+    await queryInterface.dropTable('orderArtWorks');
   }
 };
