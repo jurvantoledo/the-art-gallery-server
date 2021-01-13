e56a5deb29e4670b4866d849de60b4b036dee2f3
jurvantoledo <jurvantoledo@live.nl>
2021-01-13 12:08:34 +0100

    added images to order page

--- routers/order.js
+++ routers/order.js
@@ -29,7 +29,7 @@ router.get("/:id", async (req, res, next) => {
             {
             model: ArtWork,
             include: [{ model: Gallery, attributes: ["name"] }],
-            attributes: ["name"],
+            attributes: ["name", "image"],
             },
             User
         ],
